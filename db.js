adf9779b63f19004f2c916b6b5288ec7a31b0b8d
satyam kumar <satyarajbarnwal2008@gmail.com>
2026-03-28 01:27:59 +0530

    neon db

--- db.js
+++ db.js
@@ -1,11 +1,26 @@
-import pkg, { Pool } from 'pg'
-const {POOL} = pkg
+// import pkg, { Pool } from 'pg'
+// const {POOL} = pkg
+// import dotenv from 'dotenv'
+
+// dotenv.config()
+
+// const pool = new Pool({
+//     connectionString: process.env.URL
+// })
+
+// export default pool
+import pkg from 'pg'
 import dotenv from 'dotenv'
 
 dotenv.config()
 
+const { Pool } = pkg
+
 const pool = new Pool({
-    connectionString: process.env.URL
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 })
 
 export default pool
